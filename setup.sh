207b93568136f22ea5f71f9ff2ac48a905eabc2c
Chris King <chris.king@kilncode.com>
2018-07-19 14:54:25 -0400

    cleaned up readme and setup

--- setup.sh
+++ setup.sh
@@ -13,8 +13,6 @@ sudo iwconfig
 # The following sets up the wireless network for the device
 sudo echo "auto wlp4s0" >> /etc/network/interfaces
 sudo echo "iface wlp4s0 inet dhcp" >> /etc/network/interfaces
-sudo echo "        wpa-ssid SSID-HERE" >> /etc/network/interfaces
-sudo echo "        wpa-psk WPA-PASSCODE-HERE" >> /etc/network/interfaces
 
 # Finally reboot the device and it should automatically be connected to the wifi network.
 sudo reboot
